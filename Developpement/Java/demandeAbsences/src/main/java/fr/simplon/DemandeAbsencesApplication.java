d361ee59338d1731a0c4769a59f85aec7eed113e
Jean-at-simplon <jeanlefrancois.simplon@gmail.com>
2017-04-27 09:55:51 +0200

    Jean - Mise en place du fichier Config

--- Developpement/Java/demandeAbsences/src/main/java/fr/simplon/DemandeAbsencesApplication.java
+++ Developpement/Java/demandeAbsences/src/main/java/fr/simplon/DemandeAbsencesApplication.java
@@ -3,10 +3,10 @@ package fr.simplon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
 public class DemandeAbsencesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemandeAbsencesApplication.class, args);
+		SpringApplication.run(Config.class, args);
 	}
 }
