e95b8adcc8da0e7f2f61a5447e24f1613931d95d
le19111967 <le19111967@gmail.com>
2017-04-28 09:20:06 +0200

    Fred - Maj BDD

---
+++ Developpement/Java/demandeAbsences/src/main/java/fr/simplon/domain/Type.java
@@ -0,0 +1,51 @@
+package fr.simplon.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+@Table(name = "Type")
+public class Type {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", unique = true, nullable = false)
+	private Long id;
+	
+	@Column(name = "Nom")
+	@NotBlank(message = "Nom obligatoire")
+	private String nom;
+
+
+
+	public Type(Long id, String nom) {
+		super();
+		this.id = id;
+		this.nom = nom;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+}
