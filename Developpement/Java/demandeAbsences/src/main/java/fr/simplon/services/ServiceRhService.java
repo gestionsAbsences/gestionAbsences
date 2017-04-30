5b071a8a5da5231c2a3d203c2b6694ccde69a0e5
Jean-at-simplon <jeanlefrancois.simplon@gmail.com>
2017-04-30 09:08:36 +0200

    Jean - Module Service Rh fonctionnel + mise à jour BDD

--- Developpement/Java/demandeAbsences/src/main/java/fr/simplon/services/ServiceRhService.java
+++ Developpement/Java/demandeAbsences/src/main/java/fr/simplon/services/ServiceRhService.java
@@ -1,74 +1,153 @@
 package fr.simplon.services;
 
-import java.sql.Timestamp;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import fr.simplon.dao.ServiceRhDao;
 import fr.simplon.domain.ServiceRh;
 
 /**
- * Classe gérant le service RH
+ * Classe métier du service Rh
  * 
  * @author JGL
  *
  */
 @Service
+@Transactional
 public class ServiceRhService {
-	
+
 	@Autowired
 	private ServiceRhDao rhDao;
-
-	public Iterable<ServiceRh> listServicesRh(String search) throws Exception {
+	
+	/**
+	 * Liste des services Rh
+	 * @return une liste
+	 * @throws SQLException
+	 */
+	/*
+	 * La methode [findAll] retourne une iteration de la table
+	 * Avec la boucle [for], on la parcours et on retourne une
+	 * liste de la table
+	 */
+	public List<ServiceRh> listeServicesRh() throws SQLException {
+		List<ServiceRh> resultat = new ArrayList<>();
 		try {
-			if (! "".equals(search))
-				return rhDao.findServiceRh(search);
-			
-			else
-				return rhDao.findAll();
+			Iterable<ServiceRh> recherche = rhDao.findAll();
+
+			for (ServiceRh serviceRh : recherche) {
+				ServiceRh rh = new ServiceRh();
+				rh.setId(serviceRh.getId());
+				rh.setEmail(serviceRh.getEmail());
+				rh.setNom(serviceRh.getNom());
+				resultat.add(rh);
+			}
 		} catch (Exception e) {
-			System.out.println("Hibernate Error !: listServiceRh"+ e);
+			System.out.println("Hibernate Error !: listeServiceRh" + e);
 			throw e;
 		}
+		return resultat;
 	}
 
-	public ServiceRh getServiceRh(Long id) throws Exception {
-		ServiceRh serviceRh = null;
+	/**
+	 * Recherche d'un service Rh
+	 * @param nom
+	 * @return une liste des services rh en fonction du nom
+	 * @throws SQLException
+	 */
+	/*
+	 * Meme principe que ci-dessus
+	 * une iteration qu'on transforme en liste
+	 */
+	public List<ServiceRh> getServiceRh(String nom) throws SQLException {
+		List<ServiceRh> resultat = new ArrayList<>();
 		try {
-			serviceRh = rhDao.findOne(id);
+			Iterable<ServiceRh> recherche = rhDao.findByName(nom);
+	
+			for (ServiceRh serviceRh : recherche) {
+				ServiceRh rh = new ServiceRh();
+				rh.setId(serviceRh.getId());
+				rh.setEmail(serviceRh.getEmail());
+				rh.setNom(serviceRh.getNom());
+				resultat.add(rh);
+			}
 		} catch (Exception e) {
-			System.out.println("Hibernate Error !: getServiceRh"+ e);
+			System.out.println("Hibernate Error !: listeServiceRh" + e);
 			throw e;
 		}
-		return serviceRh;
+		return resultat;
 	}
 
-	public ServiceRh insertServiceRh(ServiceRh serviceRh) throws Exception {
+	/**
+	 * Creation nouveau service Rh
+	 * @param serviceRh
+	 * @return objet
+	 * @throws SQLException
+	 */
+	/*
+	 * Simple methode hibernate pour la creation d'un nouveau service Rh
+	 * J'ai crée un bojet ServiceRh pour avoir le resultat de la creation en retour
+	 */
+	public ServiceRh insertServiceRh(ServiceRh serviceRh) throws SQLException {
+		ServiceRh creation = new ServiceRh();
 		try {
-			serviceRh.setId(new Long(0));
-			serviceRh = rhDao.save(serviceRh);
+			creation = rhDao.save(serviceRh);
 		} catch (Exception e) {
-			System.out.println("Hibernate Error !: insertServiceRh"+ e);
+			System.out.println("Hibernate Error !: insertServiceRh" + e);
 			throw e;
 		}
-		return serviceRh;
+		return (ServiceRh) creation;
 	}
 
-	public void updateServiceRh(ServiceRh serviceRh) throws Exception {
+	/**
+	 * Modification service Rh
+	 * @param serviceRh
+	 * @return Objet
+	 * @throws SQLException
+	 */
+	/*
+	 * Même principe que creation
+	 */
+	public ServiceRh updateServiceRh(ServiceRh serviceRh) throws SQLException {
+		ServiceRh modif = new ServiceRh();
 		try {
-			rhDao.save(serviceRh);
+			modif = rhDao.save(serviceRh);
 		} catch (Exception e) {
-			System.out.println("Hibernate Error !: updateServiceRh"+ e);
+			System.out.println("Hibernate Error !: updateServiceRh" + e);
 			throw e;
 		}
+		return (ServiceRh) modif;
 	}
 
-	public void deleteServiceRh(Long id) throws Exception {
-		try {
-			rhDao.delete(id);
+	/**
+	 * Suppression Service Rh
+	 * @param serviceRh
+	 * @throws SQLException
+	 */
+	/*
+	 * On commence par faire une recherche d'un service rh
+	 * avec la methode [findByName()]
+	 * Et on supprime l'objet par la methode delete
+	 * d'hibernate qui supprime une entité complete.
+	 * Cette methode peut etre appelé à evoluer
+	 */
+	public void deleteServiceRh(ServiceRh serviceRh) throws SQLException {
+		try{
+			Iterable<ServiceRh> temp = rhDao.findByName(serviceRh.getNom());
+			ServiceRh rh = new ServiceRh();
+			for (ServiceRh service : temp) {
+				
+				rh.setId(service.getId());
+				rh.setEmail(service.getEmail());
+				rh.setNom(service.getNom());
+			}
+			rhDao.delete(rh);
 		} catch (Exception e) {
-			System.out.println("Hibernate Error !: deleteServiceRh"+ e);
+			System.out.println("Hibernate Error !: deleteServiceRh" + e);
 			throw e;
 		}
 	}
