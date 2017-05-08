3a13ebe7a222e3f62656c3b8549233050f6e4a2b
PC Maison <le19111967@gmail.com>
2017-05-08 17:21:56 +0200

    Fred - Base de test + Cardinalités

--- Developpement/Java/demandeAbsences/src/main/java/fr/simplon/domain/Absence.java
+++ Developpement/Java/demandeAbsences/src/main/java/fr/simplon/domain/Absence.java
@@ -15,7 +15,7 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 /**
- * entity Service RH
+ * entity Service Absence
  * 
  */
 
