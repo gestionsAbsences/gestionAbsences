f67ccaeb29ae81fc947ceaa11a9c7e4b8a42a5d2
PC Maison <le19111967@gmail.com>
2017-05-22 07:55:53 +0200

    Fred - React

---
+++ Developpement/React/demandeAbsences/src/Site/Entete/Entete.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import '../Bootstrap/dist/css/sb-admin-2.css'
+import './entete.css';
+
+import logo from '../Medias/Logo.jpg';
+
+let nom   = "WALTER";
+let prenom  = "Lisandra";
+let matricule = "GDUW23";
+
+class Entete extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <div className = "header breadcrumb">
+            <img className="logo" src={logo} />
+            {nom} {prenom} - {matricule}
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Entete;
