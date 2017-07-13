5cff24e86da308cb94aa60712e17502bd4ba15ad
PC Simplon <le19111967@gmail.com>
2017-07-13 13:25:58 +0200

    Fred - Adjust

--- Developpement/Java/demandeAbsences/src/main/java/fr/simplon/webSecurity/configuration/AuthSuccessHandler.java
+++ Developpement/Java/demandeAbsences/src/main/java/fr/simplon/webSecurity/configuration/AuthSuccessHandler.java
@@ -1,24 +1,24 @@
-package fr.simplon.webSecurity.configuration;
-
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-/**
- * Securité de l'application
- */
-public class AuthSuccessHandler implements AuthenticationSuccessHandler {
-
-	@Override
-	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response
-			, Authentication Auth) throws IOException, ServletException {
-		response.setStatus(HttpServletResponse.SC_OK);
-
-	}
-
-}
+//package fr.simplon.webSecurity.configuration;
+//
+//import java.io.IOException;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+//
+///**
+// * Securité de l'application
+// */
+//public class AuthSuccessHandler implements AuthenticationSuccessHandler {
+//
+//	@Override
+//	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response
+//			, Authentication Auth) throws IOException, ServletException {
+//		response.setStatus(HttpServletResponse.SC_OK);
+//
+//	}
+//
+//}
